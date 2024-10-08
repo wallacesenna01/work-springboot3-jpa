c30c2f75c9122a5a6c8a924cc01302334755bf72
Wallace Artur <wallacearthur_3@hotmail.com>
2024-10-08 09:26:55 -0300

    OrderItem, many to many, association with extra atributes part2

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -3,7 +3,6 @@ package com.educandoweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
